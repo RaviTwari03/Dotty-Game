b976eda445fe95970ce7662c344836d2430b8026
RaviTwari03 <ravirkt0@gmail.com>
2025-07-14 22:29:00 +0530

    Initial Commit

---
+++ Random Game/Random_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_GameApp.swift
+//  Random Game
+//
+//  Created by Ravi Tiwari on 14/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Random_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
